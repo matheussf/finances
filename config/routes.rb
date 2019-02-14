17862bf3fd732655f7903cc283233d3636227c3c
matheussf <matheussf.sc@gmail.com>
2019-02-14 13:02:18 +0000

    Created Pages and Routing to login/register/logout - Deleted old tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#index'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 end
